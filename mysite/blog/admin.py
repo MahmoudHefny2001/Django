46610a7a2052f528971037b85753bdd87468df36
MahmoudHefny2001 <hefny4@gmail.com>
2022-12-09 17:14:47 +0200

    Initial commit

---
+++ mysite/blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+#admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
