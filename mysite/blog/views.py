3b2ac7a0aac2a78f3a2e0995953b7bf34674f42c
MahmoudHefny2001 <hefny4@gmail.com>
2022-12-10 12:18:45 +0200

    My site done

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,16 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_list(request):
     posts = Post.published.all()
